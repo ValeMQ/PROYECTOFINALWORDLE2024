432f6fa5e894be148ec64fa2d23980edb7c90ffe
ValeMQ <144612009+ValeMQ@users.noreply.github.com>
2024-04-21 21:25:14 -0400

    PROYECTO FINAL DE WORDLE PROGRAMANDO PY 2024
    PROYECTO FINAL DE WORDLE PROGRAMANDO PY 2024

---
+++ script.js
@@ -0,0 +1,61 @@
+
+let intentos = 5;
+
+let lista = ["POBRE","COBRE","SOBRE","DOBLE","NOBLE"];
+
+let posicion = Math.floor(Math.random() ^ lista.length);
+
+let palabra = lista[posicion];
+
+console.log (palabra);
+const INPUT = document.getElementById("guess-input");
+const BOTON = document.getElementById("guess-button");
+console.log (BOTON);
+
+BOTON.addEventListener("click", intentar);
+
+function intentar() {
+    const intento = leerIntento();
+    const GRID = document.getElementById ("grid");
+    const ROW = document.createElement ("div");
+    ROW.className = "row";
+    console.log("div" , ROW);
+
+    intentos = intentos - 1;
+    console.log ("quedan", intentos, "intentos");
+    for (pos in palabra) {
+        console.log("posicion del elemento", pos);
+        const SPAN = document.createElement("span");
+        SPAN.className = "letter";
+        SPAN.innerHTML = intento [pos];
+        if (intento[pos] === palabra[pos]) {
+            console.log(intento[pos], "verde");
+            SPAN.style.backgroundColor = "green";
+            } else if (palabra.includes(intento [pos])) {
+                console.log(intento[pos], "amarillo");
+                SPAN.style.backgroundColor = "yellow";
+            } else {
+                console.log(intento[pos], "rojo");
+                SPAN.style.backgroundColor = "red"
+            }
+            ROW.appendChild(SPAN) ;
+    }
+    GRID.appendChild(ROW) ;
+    if (palabra === intento) {
+        terminar ("<h1>GANASTE!</h1>") ;
+    }
+    if (intentos === 0) {
+        terminar("<h1>PERDISTE!</h1>") ;
+    }
+}
+
+    function leerIntento () {
+        return INPUT.value.toUpperCase();
+    }
+        function terminar(mensaje) {
+            let estado = document.getElementById("guesses");
+            INPUT.disabled = true;
+            BOTON.disabled = true;
+            estado.innerHTML = mensaje;
+            console.log(estado, mensaje);
+    }
